a51a4688dec9f8c1ecc3f50a3e90b1cf1d33bbed
Nilson Nascimento <nnascimento@redzonemap.com>
2017-09-13 18:35:59 -0400

    Add GET /forms/:id to Form controller

--- Sources/App/Models/FormController.swift
+++ Sources/App/Models/FormController.swift
@@ -4,14 +4,17 @@ import MongoKitten
 final class FormController {
     
     static let collection = "forms"
+    static let results = "form_results"
     
     static func setupRoutes(_ droplet: Droplet) {
         
+        // GET /forms
         droplet.get(collection) { request in
             let forms = try chuvaMongoDb[collection].find()
             return forms.makeDocument().makeExtendedJSONString()
         }
         
+        // GET /forms/:id
         droplet.get(collection, ":id") { request in
             guard let id = request.parameters["id"]?.string else {
                 throw Abort.badRequest
@@ -30,6 +33,44 @@ final class FormController {
             form["users"] = users
             
             return form.makeExtendedJSONString()
+        }
+        
+        // GET /forms/:id/results
+        droplet.get(collection, ":id", "results") { request in
+            // Form id
+            guard let formId = request.parameters["id"]?.string else {
+                throw Abort.badRequest
+            }
+            
+            // Results for form id
+            let form_results = try chuvaMongoDb[results].find("form" == ObjectId(formId))
+            
+            let expandedResults: [Document] = try form_results.makeIterator().map {
+                var result = $0
+                
+                // Expand user objects
+                let userId = ObjectId(result["user"])!
+                let user = try chuvaMongoDb["users"].findOne("_id" == userId)
+                result["user"] = user
+                
+                // Expand question objects
+                var answers = Document(result["answers"])!.arrayRepresentation.map { Document($0)! }
+                let form = try chuvaMongoDb["forms"].findOne("_id" == ObjectId(formId))
+                answers = answers.map {
+                    var answer = $0
+                    let questionId = ObjectId(answer["question"])!
+                    let question = Document(form?["questions"])!.arrayRepresentation
+                        .flatMap { Document($0) }
+                        .filter { String($0["_id"]) == questionId.hexString }
+                        .first
+                    answer["question"] = question
+                    return answer
+                }
+                result["answers"] = answers
+                return result
+            }
+            
+            return expandedResults.makeDocument().makeExtendedJSONString()
         }
     }
     
