fcfd6e4871924602b6b486faaa455a4e78b8b618
Nilson Nascimento <nnascimento@redzonemap.com>
2017-09-13 21:11:37 -0400

    Add AuthProvider

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -1,4 +1,5 @@
 import MongoProvider
+import AuthProvider
 
 extension Config {
     public func setup() throws {
@@ -13,6 +14,7 @@ extension Config {
     /// Configure providers
     private func setupProviders() throws {
         try addProvider(MongoProvider.Provider.self)
+        try addProvider(AuthProvider.Provider.self)
     }
     
     /// Add all models that should have their
